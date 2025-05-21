56106128f0dc72825a235bb067a9a879c9631515
Deepak8210 <deepakrajput058@gmail.com>
2025-05-21 14:31:56 +0530

    recent activity in descending order

--- controllers/recentActivity.js
+++ controllers/recentActivity.js
@@ -11,7 +11,9 @@ const createActivity = async (payload) => {
 
 const getAllRecentActivities = async (req, res) => {
   try {
-    const recentActivities = await RecentActivity.find();
+    const recentActivities = await RecentActivity.find().sort({
+      createdAt: -1,
+    });
     res.status(200).json(recentActivities);
   } catch (error) {
     console.error(error);
